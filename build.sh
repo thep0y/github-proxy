6d91b8fe9eabed73a2a606ccce677523c45d3d01
thepoy <thepoy@163.com>
2025-03-28 14:24:08 +0800

    refactor(build.sh): change tar command to work from build directory
    Modify the build script to change the working directory before creating the tar archive. This ensures the tar command correctly includes the binary without the build directory path in the archive.

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ set -- arm64 amd64
 
 for arch in "$@"; do
 	CGO_ENABLED=0 GOOS='linux' GOARCH=$arch go build -ldflags="-s -w" -o "build/gp" main.go
-  tar czf "build/gp_${version}_linux_${arch}.tar.gz" build/gp
+  cd build
+  tar czf "gp_${version}_linux_${arch}.tar.gz" gp
+  cd ..
 done
