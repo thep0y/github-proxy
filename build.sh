82a0cc56fb0bd1e41b0331d6e0ea6e4642261c12
thepoy <thepoy@163.com>
2025-03-28 14:19:25 +0800

    build: update build script and .gitignore to use `build` directory

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ version=$1
 set -- arm64 amd64
 
 for arch in "$@"; do
-	CGO_ENABLED=0 GOOS='linux' GOARCH=$arch go build -ldflags="-s -w" -o "dist/gp_${version}_linux_${arch}" main.go
+	CGO_ENABLED=0 GOOS='linux' GOARCH=$arch go build -ldflags="-s -w" -o "build/gp" main.go
+  tar czf "build/gp_${version}_linux_${arch}.tar.gz" build/gp
 done
